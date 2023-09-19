0b0046f61fdd5644432433278ecdbee897ac334c
Hemayet Mondol <hemayetmondol100@gmail.com>
2023-09-19 18:01:10 +0600

    Employee Form Designed on View

--- src/FrontEnd/FristApp/Models/Employee.cs
+++ src/FrontEnd/FristApp/Models/Employee.cs
@@ -19,6 +19,8 @@ namespace FristApp.Models
         /// <value>
         /// The name.
         /// </value>
+        /// 
+        [Display(Name ="Eployee Name")]
         public string EmployeeName { get; set; } = string.Empty;
 
         /// <summary>
